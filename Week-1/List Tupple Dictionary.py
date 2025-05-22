2ca314f8b075a39b7bdce4ec3eccd42cf86292c7
Ronock Razin Jarif <razinzarif2109@gmail.com>
2025-05-22 22:51:27 +0600

    Add files via upload

---
+++ Week-1/List Tupple Dictionary.py
@@ -0,0 +1,24 @@
+# List 
+data=[4,5,6,7,8]
+data.append(9)
+data.remove(4)
+print(data)
+
+
+# Tuple
+data1=(4,5,6,7)
+new_data1=list(data1)
+new_data1.append(91)
+new_data1.remove(4)
+print(new_data1)
+
+
+# Dictionary
+person = {"name":"John"}
+person["age"]=30
+person["name"]="John Doe"
+del person["age"]
+print(person)
+
+
+
