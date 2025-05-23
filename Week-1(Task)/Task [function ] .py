986bf9c6a2a72be314d6abafb596ca6a5d23688b
Ronock Razin Jarif <razinzarif2109@gmail.com>
2025-05-23 23:40:40 +0600

    Add files via upload

---
+++ Week-1(Task)/Task [function ] .py
@@ -0,0 +1,18 @@
+# NID Information
+def get_name(fname,mname,lname):
+    print(fname+" "+mname+" "+lname)
+def get_presentadd(address):
+    print(address)
+def get_permanentadd(paddress):
+    print(paddress)
+def get_dateofbirth(date,month,year):
+    print(date+" "+month+" "+year)
+def get_gender(gender):
+    print(gender)
+
+get_name("Ronock","Razin","Jarif")
+get_presentadd("Mirpur-14, Dhaka cantonment")
+get_permanentadd("Meherpur, Kushtia")
+get_dateofbirth("9","June","2003")
+get_gender("male")
+
